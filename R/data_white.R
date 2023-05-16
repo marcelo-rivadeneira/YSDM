c669b4d147913c50d3c71968526da5eaaa61337d
marcelo-rivadeneira <122471839+marcelo-rivadeneira@users.noreply.github.com>
2023-05-16 11:32:22 -0400

    Add files via upload

---
+++ R/data_white.R
@@ -0,0 +1,16 @@
+#' White Shark Occurrences
+#'
+#' Global georeferenced occurrences for the great white shark. Thinned to avoid duplicated coordinates
+#'
+#' @format data.frame with thre columns
+#'  \describe{
+#'      \item{x}{longitude (decimal)}
+#'      \item{y}{latitude (decimal)}
+#'      }
+#'
+#' @source OBIS
+#'
+#' @examples
+#' data(data_white)
+#'
+"whiteshark"
